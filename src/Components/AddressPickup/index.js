97ca8f41b41fcf19e98d56ac80a9eb3df106d1e8
umakunal <umakunal11@gmail.com>
2023-07-11 15:15:22 +0530

    Fetching City name and zipcode is added

--- src/Components/AddressPickup/index.js
+++ src/Components/AddressPickup/index.js
@@ -7,10 +7,35 @@ import {GoogleMapKey} from '../../Constants/GoogleMapKey';
 // create a component
 const AddressPickup = ({placeholderText, fetchAddress}) => {
   const onPressAddress = (data, details) => {
+    console.log('Details', details);
+    let resLength = details.address_components;
+    let zipcode = '';
+    let filterResCity = details.address_components.filter(val => {
+      if (val.types.includes('locality') || val.types.includes('sublocality')) {
+        return val;
+      }
+      if (val.types.includes('postal_code')) {
+        let postalCode = val.long_name;
+        zipcode = postalCode;
+      }
+
+      return false;
+    });
+    let dataTextCityObject =
+      filterResCity.length > 0
+        ? filterResCity[0]
+        : details.address_components[
+            resLength > 1 ? resLength - 2 : resLength - 1
+          ];
+    let cityText =
+      dataTextCityObject.long_name && dataTextCityObject.long_name.length > 17
+        ? dataTextCityObject.short_name
+        : dataTextCityObject.long_name;
+    console.log('zipCode=====>', zipcode);
+    console.log('cityText=====>', cityText);
     const lat = details.geometry.location.lat;
     const lng = details.geometry.location.lng;
-    fetchAddress(lat, lng);
-    // console.log('Details', details);
+    fetchAddress(lat, lng, zipcode, cityText);
   };
   return (
     <View style={styles.container}>
--- src/Screens/ChooseLocation/index.js
+++ src/Screens/ChooseLocation/index.js
@@ -1,59 +1,62 @@
 //import liraries
-import React, { Component, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React, {Component, useState} from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import AddressPickup from '../../Components/AddressPickup';
 import CustomButton from '../../Components/CustomButton';
-import { useNavigation } from '@react-navigation/native';
-import { showError, showSucess } from '../../Helper/ToastMessage';
+import {useNavigation} from '@react-navigation/native';
+import {showError, showSucess} from '../../Helper/ToastMessage';
 
 // create a component
-const ChooseLocation = (props) => {
+const ChooseLocation = props => {
   const navigation = useNavigation();
   const [state, setState] = useState({
     pickupCords: {},
     destinationCords: {},
-  })
+  });
 
-  const { destinationCords } = state
+  const {destinationCords} = state;
   const checckValid = () => {
     if (Object.keys(destinationCords).length === 0) {
-      showError('Please select destination location')
-      return false
+      showError('Please select destination location');
+      return false;
     }
-    return true
-  }
+    return true;
+  };
 
-  console.log('props', props)
+  console.log('props', props);
   const onDone = () => {
-    const isValid = checckValid()
-    console.log('isValid', isValid)
+    const isValid = checckValid();
+    console.log('isValid', isValid);
     if (isValid) {
-      props.route.params.getCordinates({ destinationCords })
-      showSucess('Location selected')
+      props.route.params.getCordinates({destinationCords});
+      showSucess('Location selected');
       navigation.goBack();
     }
-  }
+  };
 
-  const fetchDestinationCords = (lat, lng) => {
-    console.log(" latitude: ", lat);
-    console.log(" longitude: ", lng);
+  const fetchDestinationCords = (lat, lng, zipcode, city) => {
+    console.log(' zipcode: ', zipcode);
+    console.log(' city: ', city);
+    // console.log(" latitude: ", lat);
+    // console.log(" longitude: ", lng);
     setState({
       ...state,
       destinationCords: {
         latitude: lat,
         longitude: lng,
       },
-    })
-  }
+    });
+  };
   return (
     <View style={styles.container}>
-      <ScrollView keyboardShouldPersistTaps="handled"
-        style={{ flex: 1, backgroundColor: 'white', padding: 20 }}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        style={{flex: 1, backgroundColor: 'white', padding: 20}}>
         <AddressPickup
-          placeholderText={"Search your destination location"}
+          placeholderText={'Search your destination location'}
           fetchAddress={fetchDestinationCords}
         />
-        <CustomButton btnText={"Done"} onPress={onDone} />
+        <CustomButton btnText={'Done'} onPress={onDone} />
       </ScrollView>
     </View>
   );
